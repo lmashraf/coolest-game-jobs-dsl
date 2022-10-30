86bc0ca0f7ac02e1c5585239438833ff4cc4fc0f
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2022-10-30 12:25:32 +0100

    Added content to the groovy files.

---
+++ nv-test-coolest-game.groovy
@@ -0,0 +1,21 @@
+pipelineJob('nv-build-coolest-game')
+{
+    def git_repository = 'https://${personal_token}:github.com/lmashraf/coolest-game'
+
+    description("Pipeline to run Nightly Verification pipeline for Coolest Game Project")
+
+    definition
+    {
+        cpsScm {
+            scm {
+                git {
+                    remote { url(git_repository) }
+                    scriptPath('./Jenkinsfile')
+                    branch('*/master')
+                    // Prevents tagging the repo
+                    extensions {}
+                }
+            }
+        }
+    }
+}
---
+++ rcv-build-coolest-game.groovy
@@ -0,0 +1,21 @@
+pipelineJob('rcv-build-coolest-game')
+{
+    def git_repository = 'https://${personal_token}:github.com/lmashraf/coolest-game'
+
+    description("Pipeline to run Release Candidate Verification pipeline for Coolest Game Project")
+
+    definition
+    {
+        cpsScm {
+            scm {
+                git {
+                    remote { url(git_repository) }
+                    scriptPath('./Jenkinsfile')
+                    branch('*/master')
+                    // Prevents tagging the repo
+                    extensions {}
+                }
+            }
+        }
+    }
+}
